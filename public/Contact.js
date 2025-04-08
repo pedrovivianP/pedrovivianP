4ff8e2521561e7ccf9f3681dc0d70ff9e7dfb7a4
pedrovivianP <pedro.vivian@cs.cruzeirodosul.edu.br>
2025-04-08 20:26:44 -0300

    syntax error

--- public/Contact.js
+++ public/Contact.js
@@ -7,7 +7,7 @@ const ContactSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     number: { type: String, required: true }, 
-    address: { type: String, required: true } 
+    address: { type: String, required: true },
     favorite: { type: Boolean, default: false } // novo campo
 });
 
