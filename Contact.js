7ca72e817befd9c715feb9e71fa71c933fec429f
pedrovivianP <pedro.vivian@cs.cruzeirodosul.edu.br>
2025-04-08 19:43:02 -0300

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,15 @@
+//O SITE ESTÁ RODANDO NA HOST ONRENDER COM O BANCO DE DADOS MONGODB:   https://listadecontatos.onrender.com/
+//O SITE ESTÁ RODANDO NA HOST ONRENDER COM O BANCO DE DADOS MONGODB:   https://listadecontatos.onrender.com/
+//O SITE ESTÁ RODANDO NA HOST ONRENDER COM O BANCO DE DADOS MONGODB:   https://listadecontatos.onrender.com/
+const mongoose = require('mongoose');
+
+const ContactSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    number: { type: String, required: true }, 
+    address: { type: String, required: true } 
+});
+
+const Contact = mongoose.model('Contact', ContactSchema);
+
+module.exports = Contact;
